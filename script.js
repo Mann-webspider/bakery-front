843a14e0020cd65664470d973756622ba95e0a67
Shelby_mann <maandalsania11@gmail.com>
2023-05-28 10:59:27 +0530

    added loader

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 const stripePublicKey = 'pk_test_51N9YCGSEIF6HvjniEsEYPlLdt6aeJ9jASELIrnTn2kyQ6DAyFQoQqibg4vaAaEVxaJ7Xd8tufAqvNA56HXlGGn4L00gYKnGD80';
 const stripe = Stripe(stripePublicKey);
-
+const loader = document.querySelector(".loader");
 async function sendItem(id){
+    loader.classList.remove("hidden")
     const quan = document.querySelector(`#${id}`).value
     console.log("i clicked");
     const res =await fetch("https://bakery-topaz-rho.vercel.app/create-checkout-session",{
@@ -19,16 +20,20 @@ async function sendItem(id){
         })
        
         
+    }).then((response)=>{
+        return response.json();
+
+    }).then((data)=>{
+        const {sessionId} = data;
+         stripe.redirectToCheckout({
+              sessionId: sessionId
+            });
+    }).catch((e)=>{
+        console.log(e);
+    }).finally(()=>{
+        loader.classList.add("hidden")
+
+        console.log(sessionId);
     })
-    const { sessionId } = await res.json();
-    console.log(sessionId);
-    await stripe.redirectToCheckout({
-          sessionId: sessionId
-        });
 
 }
-
-
-
-
-// pay.addEventListener("click",sendItem())
